64e90d32dd014707855c8dceb7588f9e5ff55fe0
Freddy Gonzalez <fredo_y@hotmail.com>
2021-05-05 17:14:44 -0600

    elimine las carpetas y archivos que no se utilizaban

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ var express = require('express'),
     faviconURL = `${__dirname}/public/img/favicon.ico`,
     //declaro la carpeta publica como un archivo statico
     publicDIR = express.static(`${__dirname}/public`),
-    viewDIR= `${__dirname}/views`,
-    port = (process.env.PORT ||3000),
+    viewDIR = `${__dirname}/views`,
+    port = (process.env.PORT || 3000),
     app = express()
 //configurando app
-    app
-        .set('views', viewDIR)
-        .set('view engine', 'pug')
-        .set('port', port)
+app
+    .set('views', viewDIR)
+    .set('view engine', 'pug')
+    .set('port', port)
 //ejecutando midleware
-       app
-            .use(favicon (faviconURL) )
-            .use( bodyParser.json() )
-            //toda la informacion del formulario se parse application/x-www-form-urlencoded
-            .use( bodyParser.urlencoded( { extended: true}) )
-            .use( morgan('dev') )
-            .use ( publicDIR )
-        // ejecuto el midleware enrutador
-            .use(routes)          
+app
+    .use(favicon(faviconURL))
+    .use(bodyParser.json())
+    //toda la informacion del formulario se parse application/x-www-form-urlencoded
+    .use(bodyParser.urlencoded({ extended: true }))
+    .use(morgan('dev'))
+    .use(publicDIR)
+    // ejecuto el midleware enrutador
+    .use(routes)
 module.exports = app
